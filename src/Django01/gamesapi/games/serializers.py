874b15aaa2d398689561aedfbf7ea4076c5815e6
cnpoe <cnpoek@gmail.com>
2018-03-13 22:49:56 -0600

    Serializers based in HyperlinkedModelSerializer

--- src/Django01/gamesapi/games/serializers.py
+++ src/Django01/gamesapi/games/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Game
-
+from .models import GameCategory
+from .models import PlayerScore
+from .models import Player
 
 # class GameSerializer(serializers.Serializer):
 #     pk = serializers.IntegerField(read_only=True)
@@ -21,13 +23,96 @@ from .models import Game
 #         return instance
 #
 
-class GameSerializer(serializers.ModelSerializer):
+
+# class GameSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Game
+#         fields = (
+#             'id',
+#             'name',
+#             'release_date',
+#             'game_category',
+#             'played',
+#         )
+
+
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+    games = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='game-detail'
+    )
+
+    class Meta:
+        model = GameCategory
+        fields = (
+            'url',
+            'pk',
+            'name',
+            'games'
+        )
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    game_category = serializers.SlugRelatedField(queryset=GameCategory.objects.all(), slug_field='name')
+
     class Meta:
         model = Game
         fields = (
-            'id',
+            'url',
+            'game_category',
             'name',
             'release_date',
-            'game_category',
-            'played',
+            'played'
+        )
+
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+    game = GameSerializer()
+
+    class Meta:
+        model = PlayerScore
+        fields = (
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'game'
+        )
+
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    scores = ScoreSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(
+        choices=Player.GENDER_CHOICES
+    )
+    gender_description = serializers.ChoiceField(
+        source='get_gender_display',
+        read_only=True
+    )
+
+    class Meta:
+        model = Player
+        fields = (
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'scores'
+        )
+
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+    player = serializers.SlugRelatedField(queryset=Player.objects.all(), slug_field='name')
+    game = serializers.SlugRelatedField(queryset=Game.objects.all(), slug_field='name')
+
+    class Meta:
+        model = PlayerScore
+        fields = (
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'player',
+            'game'
         )
